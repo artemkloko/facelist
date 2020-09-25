d3f5fab63045def7bb41a829e9f0836671fd16ed
artemkloko <artemkloko@gmail.com>
2020-09-25 20:11:59 +0300

    [client] Added propsAreEqual to FaceList and FaceListItem components

--- client/src/components/FacesList.tsx
+++ client/src/components/FacesList.tsx
@@ -9,27 +9,43 @@ export type FaceListProps = Pick<
   "data" | "onRefresh" | "onEndReached" | "refreshing"
 >;
 
-export const FacesList = memo<FaceListProps>((props) => {
-  return (
-    <FlatList
-      data={props.data}
-      renderItem={({ item }) => <FacesListItem item={item} key={item.id} />}
-      onRefresh={props.onRefresh}
-      onEndReached={props.onEndReached}
-      refreshing={props.refreshing}
-    />
-  );
-});
+export const FacesList = memo<FaceListProps>(
+  (props) => {
+    return (
+      <FlatList
+        data={props.data}
+        renderItem={({ item }) => <FacesListItem item={item} key={item.id} />}
+        onRefresh={props.onRefresh}
+        onEndReached={props.onEndReached}
+        refreshing={props.refreshing}
+      />
+    );
+  },
+  (prevProps, nextProps) => {
+    const same =
+      prevProps.data === nextProps.data &&
+      prevProps.refreshing === nextProps.refreshing;
+    const shouldRender = !nextProps.refreshing && !same;
+    return !shouldRender;
+  }
+);
 
 type FacesListItemProps = { item: FaceFragment };
 
-export const FacesListItem = memo<FacesListItemProps>((props) => {
-  return (
-    <ListItem bottomDivider>
-      <Avatar source={{ uri: props.item.avatar }} />
-      <ListItem.Content>
-        <ListItem.Title>{props.item.name}</ListItem.Title>
-      </ListItem.Content>
-    </ListItem>
-  );
-});
+export const FacesListItem = memo<FacesListItemProps>(
+  (props) => {
+    return (
+      <ListItem bottomDivider>
+        <Avatar source={{ uri: props.item.avatar }} />
+        <ListItem.Content>
+          <ListItem.Title>{props.item.name}</ListItem.Title>
+        </ListItem.Content>
+      </ListItem>
+    );
+  },
+  (prevProps, nextProps) => {
+    const same = prevProps.item === nextProps.item;
+    const shouldRender = !same;
+    return !shouldRender;
+  }
+);
--- client/src/screens/FacelistScreen.tsx
+++ client/src/screens/FacelistScreen.tsx
@@ -12,6 +12,8 @@ export const FacelistScreen: React.FC = () => {
 
   useEffect(() => {
     query.execute();
+
+    return () => {};
   }, []);
 
   return (
